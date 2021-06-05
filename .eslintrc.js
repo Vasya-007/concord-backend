1de3ab612453dd01efc64cd0c7904e198210647f
Vasya-007 <boyb94115@gmail.com>
2021-06-05 21:03:23 +0300

    commit3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  parser: "@babel/eslint-parser",
+  parser: '@babel/eslint-parser',
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  extends: ['airbnb-base', 'plugin:prettier/recommended', 'eslint:recommended'],
   parserOptions: {
-    sourceType: "module",
+    sourceType: 'module',
   },
   rules: {
-    "no-use-before-define": 0,
-    "no-underscore-dangle": 0,
+    'no-use-before-define': 0,
+    'no-underscore-dangle': 0,
   },
 };
