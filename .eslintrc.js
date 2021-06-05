2ae5f537dcf1f9f4f04b05a16fc6b68c0dc9e0db
Vasya-007 <boyb94115@gmail.com>
2021-06-05 19:39:18 +0300

    added babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
+  parser: "@babel/eslint-parser",
   env: {
     browser: true,
     es2021: true,
   },
   extends: ["airbnb-base", "plugin:prettier/recommended"],
   parserOptions: {
-    ecmaVersion: 12,
     sourceType: "module",
   },
   rules: {
-    "prettier/prettier": "error",
+    "no-use-before-define": 0,
+    "no-underscore-dangle": 0,
   },
 };
